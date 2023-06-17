44626443147d2ec8a433013d66dea86f39ef0f8c
Bishal Mukherjee <bishalmukherjee448@gmail.com>
2023-06-17 22:58:11 +0530

    [NEW] index route added.

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 8080;
 
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "SERVER WORKING" });
+});
+
 app.use("/api/users", require("./routes/users"));
 
 app.listen(PORT, () => {
